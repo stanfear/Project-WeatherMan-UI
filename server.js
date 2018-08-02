75142e4045799174e7ffd3eb7e95d7ca3a665aaf
StanFear <maldouck@hotmail.fr>
2018-08-02 07:30:34 +0200

    Updated to use SQL queries

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport'); 
@@ -5,10 +6,39 @@ const DiscordStrategy = require('passport-discord').Strategy;
 const moment = require('moment');
 const bodyParser = require('body-parser');
 const app = express();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'WeatherMan',
+    password: 'WeatherMan',
+    database: 'weatherman',
+});
+connection.config.queryFormat = function (query, values) {
+    if (!values) return query;
+    return query.replace(/\:(\w+)/g, function (txt, key) {
+        if (values.hasOwnProperty(key)) {
+            return this.escape(values[key]);
+        }
+        return txt;
+    }.bind(this));
+};
+
+
+const queryAsync = promisify(connection.query).bind(connection);
+
+
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
 
 
 const params = require("./params.json");
-
 
 const ProjectPage = "/WeatherMan"
 
@@ -82,20 +112,83 @@ app.get(ProjectPage, checkAuth, function (req, res) {
     res.render('index', { user: req.user});
 });
 
-app.get("/cell/:id", checkAuth, function (req, res) {
-    slots = [new Date(2018, 07, 28, 15), new Date(2018, 07, 28, 16), new Date(2018, 07, 28, 17)]
+app.get("/cell/:id", checkAuth, async function (req, res) {
+    // query database
+    let rows = await queryAsync(`
+    SELECT 
+        *
+    FROM
+        weatherman.accuweather_report acr
+            JOIN
+        weatherman.cell c ON c.AccuWeatherCellId = acr.AccuWeatherCellId
+        WHERE
+        c.idcell = :cellId
+            AND NOT EXISTS( SELECT 
+                *
+            FROM
+                weatherman.user_report ur
+            WHERE
+                ur.idCell = c.idcell
+                    AND acr.reportTime = ur.ReportTime);`,
+        {cellId : req.params.id});
+    let slots = Array.from(rows, row => row.reportTime);
 
-    res.render('cell-filler', { moment : moment, slots: slots});
+    res.render('cell-filler', { moment: moment, slots: slots, cell: req.params.id});
 });
 
-app.post("/cell/:id/:slot", checkAuth, function (req, res) {
-    console.log(JSON.stringify(req.body));
+app.post("/cell/:id/:slot", checkAuth, async function (req, res) {
+
+    await queryAsync(`
+    INSERT INTO user_report (
+        idUser,
+        idCell,
+        userReportTime,
+        reportTime,
+        inGameWeather,
+        inGameEffect,
+        inGameWind)
+    SELECT 
+        idUser,
+        :cellId,
+        NOW(),
+        :slot,
+        :weather,
+        :effect,
+        :wind 
+    FROM weatherman.user u 
+    WHERE 
+        u.UserName = 'StanFyr' AND
+        u.UserDiscriminator = 7270;`,
+    {
+        cellId: req.params.id,
+        slot: new Date(req.params.slot * 1000),
+        weather: req.body.MainWeather,
+        effect: req.body.EffectWeather,
+        wind: req.body.WindDirection 
+    });
     res.sendStatus(200);
 });
 
-app.delete("/cell/:id/:slot", checkAuth, function (req, res) {
-    console.log(JSON.stringify(req.route)); 
-    console.log(JSON.stringify(req.params));
+app.delete("/cell/:id/:slot", checkAuth, async function (req, res) {
+    await queryAsync(`
+    DELETE acr FROM accuweather_report acr
+    JOIN
+        weatherman.cell c ON c.AccuWeatherCellId = acr.AccuWeatherCellId
+        WHERE
+        c.idcell = :cellId
+        AND acr.reportTime = :slot
+            AND NOT EXISTS( SELECT
+                *
+            FROM
+                weatherman.user_report ur
+            WHERE
+                ur.idCell = c.idcell
+                    AND acr.reportTime = ur.ReportTime);`,
+        {
+            cellId: req.params.id,
+            slot: new Date(req.params.slot * 1000)
+        });
+
     res.sendStatus(200);
 });
 
