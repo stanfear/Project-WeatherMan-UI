c37b64aa7f54b25424640da771cc3ddf1049dafd
StanFyr <maldouck@hotmail.fr>
2019-01-21 20:50:02 +0100

    cleanup

--- server.ts
+++ server.ts
@@ -23,6 +23,7 @@ const app: express.Application = express();
 db.sequelize.sync();
 
 app.locals.db = db;
+app.locals.config = config;
 
 passport.serializeUser((user: types.Discord_DB_Profile, done: (err: any, id?: types.Discord_DB_Profile) => void) => {
     done(null, user);
@@ -93,98 +94,6 @@ function checkAuth(req: express.Request, res: express.Response, next: express.Ne
     res.redirect('/');
 }
 
-// app.get("/cell", checkAuth, async function (req, res) {
-    
-//     let rows = <any[]>await queryAsync(`
-//         SELECT 
-//             c.idcell
-//         FROM cell c
-//         WHERE c.iduser = :dbID`,
-//         {
-//             dbID: (<Discord_DB_Profile>req.user).DBid
-//         });
-//     let userscells = Array.from(rows, row => <any>{ id: row.idcell, geoJSON: new S2Cell(S2CellId.fromToken(row.idcell)).toGEOJSON() });
-//     res.render('cell-list', { cells: userscells });
-// });
-
-// app.get("/cell/:id", checkAuth, async function (req, res) {
-//     // query database
-//     let rows = <any[]>await queryAsync(`
-//     SELECT 
-//         DISTINCT reportTime
-//     FROM
-//         weatherman.accuweather_report acr
-//             JOIN
-//         weatherman.cell c ON c.AccuWeatherCellId = acr.AccuWeatherCellId
-//         WHERE
-//         reportTime < NOW() AND 
-//         c.idcell = :cellId
-//             AND NOT EXISTS( SELECT 
-//                 *
-//             FROM
-//                 weatherman.user_report ur
-//             WHERE
-//                 ur.idCell = c.idcell
-//                     AND acr.reportTime = ur.ReportTime);`,
-//         {cellId : req.params.id});
-//     let slots = Array.from(rows, row => row.reportTime);
-
-//     res.render('cell-filler', { moment: moment, slots: slots, cell: req.params.id});
-// });
-
-// app.post("/cell/:id/:slot", checkAuth, async function (req, res) {
-
-//     await queryAsync(`
-//     INSERT INTO user_report (
-//         idUser,
-//         idCell,
-//         userReportTime,
-//         reportTime,
-//         inGameWeather,
-//         inGameEffect,
-//         inGameWind)
-//     SELECT 
-//         :dbID,
-//         :cellId,
-//         NOW(),
-//         :slot,
-//         :weather,
-//         :effect,
-//         :wind;`,
-//     {
-//         cellId: req.params.id,
-//         slot: new Date(req.params.slot * 1000),
-//         weather: req.body.MainWeather,
-//         effect: req.body.EffectWeather,
-//         wind: req.body.WindDirection ,
-//         dbID: (<Discord_DB_Profile>req.user).DBid
-//     });
-//     res.sendStatus(200);
-// });
-
-// app.delete("/cell/:id/:slot", checkAuth, async function (req, res) {
-//     await queryAsync(`
-//     DELETE acr FROM accuweather_report acr
-//     JOIN
-//         weatherman.cell c ON c.AccuWeatherCellId = acr.AccuWeatherCellId
-//         WHERE
-//         c.idcell = :cellId
-//         AND acr.reportTime = :slot
-//             AND NOT EXISTS( SELECT
-//                 *
-//             FROM
-//                 weatherman.user_report ur
-//             WHERE
-//                 ur.idCell = c.idcell
-//                     AND acr.reportTime = ur.ReportTime);`,
-//         {
-//             cellId: req.params.id,
-//             slot: new Date(req.params.slot * 1000)
-//         });
-
-//     res.sendStatus(200);
-// });
-
 async function ExportAccuWeather() {
     let cells = await db.cells.findAll({
         where :{
@@ -225,11 +134,9 @@ async function ExportAccuWeather() {
     db.accuweather_reports.bulkCreate(values);
 }
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(config.get("listeningPort"), function () {
+    console.log('Example app listening on port ' + config.get("listeningPort"));
 });
 
-//ExportAccuWeather()
-
-//var j = schedule.scheduleJob(config.get("AccuWeatherImportCRON"), ExportAccuWeather);
-//j.on("scheduled", (arg: any[]) => console.log(`Next run of "ExportAccuWeather" Scheduled at ${arg}`))
+var j = schedule.scheduleJob(config.get("AccuWeatherImportCRON"), ExportAccuWeather);
+j.on("scheduled", (arg: any[]) => console.log(`Next run of "ExportAccuWeather" Scheduled at ${arg}`))
